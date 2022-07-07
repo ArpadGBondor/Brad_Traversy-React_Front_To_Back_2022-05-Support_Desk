5a41de93d1721d06f142677f5311b93851dbce1d
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-07-07 21:26:29 +0100

    Register User

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler, invalidPathHandler } = require('./middleware/errorMiddleware');
-
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Connect to database
+connectDB();
 
 // // Parse "application/json" incoming Request objects as a JSON Object
 // app.use(express.json());
