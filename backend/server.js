0e783e2cfc47548a7363e1bc7c29aac8ead16f3a
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-07-07 12:13:49 +0100

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
