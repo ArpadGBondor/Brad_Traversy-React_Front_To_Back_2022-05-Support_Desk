3bc2c4ea8d3269a804ae41e1856c3572d068fa03
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2022-07-08 01:11:16 +0100

    Login, JWT Token, Protected routes, Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const jsonWebToken = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 // @desc Register a new user
@@ -38,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     } else {
         res.status(400);
@@ -49,10 +51,43 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.send('Login Route');
+    const { email, password } = req.body;
+
+    // Validation
+    if (!email || !password) {
+        res.status(400);
+        throw new Error('Please include all fields');
+    }
+
+    const user = await User.findOne({ email });
+    // Check if user exists and password matches
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid credentials.');
+    }
+});
+
+const generateToken = (id) => {
+    return jsonWebToken.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
+
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+    const { _id, name, email } = req.user;
+    res.status(200).json({ id: _id, name, email });
 });
 
 module.exports = {
     registerUser,
     loginUser,
+    getMe,
 };
