1ee8a25f49996d5359f9738dd41d7b3ad7f26efc
Will Adams <bushblade@gmail.com>
2022-07-21 20:16:17 +0100

    remove unused vars

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
+require('colors');
+require('dotenv').config();
 const { errorHandler, invalidPathHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === 'production') {
     // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
+    app.get('*', (_, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'));
 } else {
-    app.get('/', (req, res) => {
+    app.get('/', (_, res) => {
         res.status(200).json({ message: 'Welcome to the Support Desk API' });
     });
 }
