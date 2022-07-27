abece802273f61ff41baafb4e74f89568fe3caa6
Will Adams <bushblade@gmail.com>
2022-07-27 08:15:28 +0100

    dry up services

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,26 +1,21 @@
 import api from '../api';
+import { createAuthConfig } from '../../utils';
 
 const API_URL = '/tickets';
 
 // Get ticket notes
 const getNotes = async (ticketId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    };
-    const response = await api.get(`${API_URL}/${ticketId}/notes`, config);
+    const response = await api.get(`${API_URL}/${ticketId}/notes`, createAuthConfig(token));
     return response.data;
 };
 
 // Create ticket note
 const createNote = async (noteText, ticketId, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    };
-    const response = await api.post(`${API_URL}/${ticketId}/notes`, { text: noteText }, config);
+    const response = await api.post(
+        `${API_URL}/${ticketId}/notes`,
+        { text: noteText },
+        createAuthConfig(token)
+    );
     return response.data;
 };
 
